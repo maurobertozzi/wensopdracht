545c44cd434d09552246b6fce89137fd9ad86881
maurobertozzi <maurobertozzi11@gmail.com>
2017-05-04 17:04:03 +0200

    tutorial part 2/6

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var radius = 10;
+var dragging = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerWidth;
+
+context.lineWidth = radius*2;
+
 var putpoint = function(e) {
+	if(dragging){
+	context.lineTo(e.clientX, e.clientY);
+	context.stroke();
 	context.beginPath();
-	context.arc(e.offsetX,e.offsetY, radius, 0, Math.PI*2);
+	context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
 	context.fill();
+	context.beginPath();
+	context.moveTo(e.clientX, e.clientY);
+}
+}
+var engage = function(e){
+	dragging = true;
+	putpoint(e);
+}
+var disengage = function(){
+	dragging = false;
+	context.beginPath();
 }
 
-canvas.addEventListener('mousedown' , putpoint);
+
+
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mouseup', disengage);
+canvas.addEventListener('mousemove' , putpoint);
