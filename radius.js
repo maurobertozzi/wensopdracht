c8bbe8e14cd9e21f40c66c14e30be8ba7aa752c1
maurobertozzi <maurobertozzi11@gmail.com>
2017-05-04 17:47:08 +0200

    tutorial part 3/6

---
+++ radius.js
@@ -0,0 +1,29 @@
+var setRadius = function(newRadius) {
+    if (newRadius < minRad) {
+        newRadius = minRad;
+    } else if (newRadius > maxRad) {
+        newRadius = maxRad;
+    }
+    radius = newRadius;
+    context.lineWidth = radius * 2;
+
+    radSpan.innerHTML = radius;
+}
+
+var minRad = 0.5,
+    maxRad = 100,
+    defaultRad = 10,
+    interval = 5;
+    radSpan = document.getElementById('radval')
+minRadius = document.getElementById('minRadius')
+plusRadius = document.getElementById('plusRadius')
+
+
+minRadius.addEventListener('click',function(){
+	setRadius(radius-interval);
+})
+plusRadius.addEventListener('click',function(){
+	setRadius(radius+interval);
+})
+
+setRadius(defaultRad);
--- script.js
+++ script.js
@@ -6,26 +6,26 @@ var dragging = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerWidth;
 
-context.lineWidth = radius*2;
+context.lineWidth = radius * 2;
 
 var putpoint = function(e) {
-	if(dragging){
-	context.lineTo(e.clientX, e.clientY);
-	context.stroke();
-	context.beginPath();
-	context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
-	context.fill();
-	context.beginPath();
-	context.moveTo(e.clientX, e.clientY);
+    if (dragging) {
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
+        context.fill();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    }
 }
+var engage = function(e) {
+    dragging = true;
+    putpoint(e);
 }
-var engage = function(e){
-	dragging = true;
-	putpoint(e);
-}
-var disengage = function(){
-	dragging = false;
-	context.beginPath();
+var disengage = function() {
+    dragging = false;
+    context.beginPath();
 }
 
 
@@ -33,4 +33,4 @@ var disengage = function(){
 
 canvas.addEventListener('mousedown', engage);
 canvas.addEventListener('mouseup', disengage);
-canvas.addEventListener('mousemove' , putpoint);
+canvas.addEventListener('mousemove', putpoint);
