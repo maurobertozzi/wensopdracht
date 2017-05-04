e7f03f779c8aa3dbb6c37832f37a4aeffbce9f1c
maurobertozzi <maurobertozzi11@gmail.com>
2017-05-04 16:38:00 +0200

    tutorial part 1/6

---
+++ script.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var radius = 10;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerWidth;
+var putpoint = function(e) {
+	context.beginPath();
+	context.arc(e.offsetX,e.offsetY, radius, 0, Math.PI*2);
+	context.fill();
+}
+
+canvas.addEventListener('mousedown' , putpoint);
